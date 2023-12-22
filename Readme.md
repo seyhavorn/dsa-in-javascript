97a1d199a88b8efd85bef7f8ee261dcac770c298
Seyha Vorn <vornseyha4758@gmail.com>
2023-12-22 23:08:19 +0700

    update Readme.md and stack.js

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Data Structures and Algorithms in JavaScript
+
+
+### Created By Seyha VORN
+See [Seyha VORN](https://github.com/seyhavorn/).
--- stack.js
+++ stack.js
@@ -1,5 +1,14 @@
+// Stacks
+// Function: push, pop, peek, length:
 var letters = [];
 
 var word = 'racecar';
 
 var rword = '';
+
+// pup letter of word into stack
+for (var i = 0; i < word.length; i++) {
+  letters.push(word[i]);
+  console.log('letters', letters);
+}
+ 
