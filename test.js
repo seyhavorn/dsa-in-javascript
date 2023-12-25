fda185c32228aa38b199e4e96201ae5cc0dccbb8
Seyha Vorn <vornseyha4758@gmail.com>
2023-12-25 14:11:48 +0700

    Update with binary search, queue, stack

--- test.js
+++ test.js
@@ -1,8 +1,38 @@
-function test() {
-  const myStr = 'seyhaVORN';
-  const re = myStr.split("").sort().join(" , ");
-  console.log(re);
-  console.log('Hello my name is Vorn Seyha');
-}
+const arr = [1, 2, 3, 4];
+let arr2 = [6, 7, 8, 9];
+const strArr = ['a', 'b', 'c'];
 
-test();
+// Using Push
+// const myPush = arr.push(100);
+
+// Using splice:
+// const mySplic = arr.splice(1,2, 100);
+
+// shift udsing for remove first index of array:
+// const myShift = arr.shift(2);
+
+// using: unshift:
+// const myUnshift = arr.unshift(1,2,3);
+
+// using concat
+
+// const myConcat = strArr.concat(arr, arr2);
+// const myJoin = arr.join('&&');
+
+// using slice:
+// const mySlice = arr.slice(2);
+
+// Using: indexOf
+// const myIndexOf = strArr.indexOf('b')
+
+// Using includes
+// const inclues = strArr.includes('gg');
+
+// Uinsg find:
+
+// const findN = arr.find((n) => n % 2 === 0);
+
+// Using map:
+const data = arr.map((n) => n * 4);
+
+console.log('my arra', data);
